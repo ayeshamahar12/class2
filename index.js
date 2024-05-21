78b25823ed2a707c2d910a211f6075ce596a8c59
ayeshamahar12 <ayeshamahar007@gmail.com>
2024-05-21 13:21:26 +0500

    best

---
+++ index.js
@@ -0,0 +1,67 @@
+// let  num = null;
+//  console.log(num);
+// Comperison operators;
+
+// ==, ===
+
+// let  num1 = 6;
+// let num2 = 6;
+// console.log(num1 == num2);
+
+// let  num1 = 6;//data types number
+// let num2 = 6;//String
+// console.log(num1 == num2);
+
+// let num1 = 6;//data types number
+// let    num2 = 6; // string
+// console.log(num1 === num2);
+
+//!=, !==
+
+// let num1 =6; //data types Number
+// let num2=7; //String
+// console.log(num1 != num2);
+
+// console.log(num1 !==  num2);
+// let num1 =55;
+// let num2 =66;
+// console.log( num1 < num2);
+
+// let num1 =155;
+// let num2= 66;
+// // console.log(num1 > num2);
+
+// let num1 =155;
+// let num2 =66;
+// console.log(num1 >= num2 );
+// let num1 = 66;
+// let num2=9999;
+// console.log(num1 <= num2);
+
+//  let usernum = + prompt("inter your number");
+//  let computernumber = Math.round(math.random()*5);
+//  if (usernum === computernumber){
+//     console.log("your win");
+
+//  }else{
+//     console.log("your loss !" +computernumber);
+//  }
+
+//  let num = false;
+//  console.log(num);
+
+// let num1 =  6;
+// let num2 = 6;
+// console.log (num1 =num2);
+
+// let num1 = 6;
+// let num2 = 6;
+// console.log(num1 == num2);
+
+let num1 =6;
+let name= 6;
+console.log(num1 === num2);
+
+
+
+
